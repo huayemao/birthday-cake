e7500c7d6db2cfd55a14859bded88f9b705617fb
huayemao <huayemao4g@gmail.com>
2025-12-22 14:43:42 +0800

    feat: 添加 favicon 等

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ export const metadata: Metadata = {
   title: `${t.seoTitle} - ${t.seoSubtitle1} | ${t.seoSubtitle2}`,
   description: t.description,
   keywords: t.keywords,
+  manifest: '/site.webmanifest',
 };
 
 const Home: React.FC = () => {
